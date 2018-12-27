49ebbceb3441152f0cb4217b4192c529d446ea6c
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-12-27 18:34:19 +0300

    Rewrite MutableList.sortWith to suppress warning instead of error
    Use available naturalOrder comparator in MutableList.sort()

--- runtime/src/main/kotlin/kotlin/collections/MutableCollections.kt
+++ runtime/src/main/kotlin/kotlin/collections/MutableCollections.kt
@@ -5,12 +5,10 @@
 
 package kotlin.collections
 
-import kotlin.comparisons.*
-
 /**
  * Sorts elements in the list in-place according to their natural sort order.
  */
-public actual fun <T : Comparable<T>> MutableList<T>.sort(): Unit = sortWith(Comparator<T> { a: T, b: T -> a.compareTo(b) })
+public actual fun <T : Comparable<T>> MutableList<T>.sort(): Unit = sortWith(naturalOrder())
 
 /**
  * Sorts elements in the list in-place according to the order specified with [comparator].
@@ -18,7 +16,7 @@ public actual fun <T : Comparable<T>> MutableList<T>.sort(): Unit = sortWith(Com
 public actual fun <T> MutableList<T>.sortWith(comparator: Comparator<in T>): Unit {
     if (size > 1) {
         val it = listIterator()
-        val sortedArray = @Suppress("TYPE_PARAMETER_AS_REIFIED") toTypedArray().apply { sortWith(comparator) }
+        val sortedArray = @Suppress("UNCHECKED_CAST") (toTypedArray<Any?>() as Array<T>).apply { sortWith(comparator) }
         for (v in sortedArray) {
             it.next()
             it.set(v)
