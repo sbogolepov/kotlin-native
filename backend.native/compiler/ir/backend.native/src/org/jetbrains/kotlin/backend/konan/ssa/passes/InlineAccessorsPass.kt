10eb762a2b0bf2161f3a09c889055b6aaa1c0b73
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2019-02-02 19:50:40 +0700

    wip inliner of trivial getters

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ssa/passes/InlineAccessorsPass.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ssa/passes/InlineAccessorsPass.kt
@@ -1,9 +1,24 @@
 package org.jetbrains.kotlin.backend.konan.ssa.passes
 
-import org.jetbrains.kotlin.backend.konan.ssa.SSAFunction
+import org.jetbrains.kotlin.backend.konan.ssa.*
 
-class InlineAccessorsPass : FunctionPass {
+class InlineAccessorsPass(private val func: SSAFunction) : FunctionPass {
     override fun apply() {
-
+        for (bb in func.blocks) {
+            for (insn in bb.body) {
+                // TODO: better filter
+                if (insn is SSAMethodCall && insn.callee.isTrivialGetter()) {
+                    val getter = insn.callee.blocks[0].body[0] as SSAGetField
+                    insn.replaceBy(SSAGetField(insn.receiver, getter.field))
+                }
+            }
+        }
     }
+
+    private fun SSAFunction.isTrivialGetter() =
+            name.contains(".<get-")
+                    && blocks.size == 1
+                    && blocks[0].body.size == 2
+                    && blocks[0].body[0] is SSAGetField
+                    && blocks[0].body[1] is SSAReturn
 }
